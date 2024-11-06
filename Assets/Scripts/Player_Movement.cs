f0c43bdddc2e020d98bb806cac55993ff8d3b676
BRuno <135856797+0WhitherGgr0@users.noreply.github.com>
2024-11-06 09:57:55 -0500

    Movimiento, escalas de autos, Limitante de espacio

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -15,17 +15,31 @@ public class Player_Movement : MonoBehaviour
 
     void Update()
     {
-
         if(Input.GetKey(KeyCode.RightArrow)) 
         { 
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
-            
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);            
         }
 
         if(Input.GetKey(KeyCode.LeftArrow)) 
         { 
-            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
-            
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);            
         }
+        // Manual way
+        if(transform.position.x < -2.58f)
+        {
+            transform.position = new Vector3(-2.58f, transform.position.y, transform.position.z);
+        }
+
+          if(transform.position.x > 2.58f)
+        {
+            transform.position = new Vector3(2.58f, transform.position.y, transform.position.z);
+        }
+        
+    
+       /* /// Unity Inbuilt feature
+        Vector3 pos = transform.position; 
+        pos.x= Mathf.Clamp(pos.x, 1.8f, 1.8f); 
+        transform.position = pos;
+*/
     }
 }
